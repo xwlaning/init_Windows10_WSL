6baca385c59c9a22468cf4b1bf130cfaceffee2f
xwlan <648613081@qq.com>
2018-10-21 00:43:08 +0800

    安装配置ssh

--- init.sh
+++ init.sh
@@ -2,3 +2,15 @@
 cp -f /etc/apt/sources.list /etc/apt/sources.list.bak
 cat ubuntu_16.04.mirror > /etc/apt/sources.list
 apt-get update
+
+# reinstall openssh-server
+echo y | apt-get remove openssh-server
+echo y | apt-get install openssh-server
+
+echo  "which port you want bind to ssh ?"
+read ssh_port
+sed -i s/"Port 22"/"Port $ssh_port"/g /etc/ssh/sshd_config
+sed -i s/"PermitRootLogin prohibit-password"/"PermitRootLogin yes"/g /etc/ssh/sshd_config
+sed -i s/"PasswordAuthentication no"/"PasswordAuthentication yes"/g /etc/ssh/sshd_config
+
+/etc/init.d/ssh start
